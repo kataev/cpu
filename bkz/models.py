adbdbf936f5dc8f7cc828704c61c13d190004cdb
Катаев Денис <bteamko@gmail.com>
2011-12-08 13:40:01 +0600

    Добавил значения в форму и добавил гитигноре

--- bkz/models.py
+++ bkz/models.py
@@ -94,15 +94,23 @@ class chartform(forms.Form):
     ('t6',u"гор 506"),
     ('t7',u"давление 401"),
     ('t8',u"разр 502"),
-    ('t9',u"разр 602"),)
+    ('t9',u"разр 602"),
+    ('t10',u"M201"),
+    ('t11',u"M202"),
+    ('t12',u"M203"),
+    ('t13',u"M304"),
+    ('t14',u"M205"),
+    ('t15',u"M206"),
+    ('t16',u"M207"),
+    ('t17',u"M208"),
+
+        )
     avg_c =(
         ('minute','Минутам'),
         ('second','Секундам'),
         ('hour','Часам'),
         ('day','Дням'),
     )
-
-
 
     model = forms.ChoiceField(label=u'Датчик', choices=model_c,)
     value = forms.ChoiceField(choices=value_c,label=u'Значения, и горелки для термодата')
